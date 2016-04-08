fd31fb783b233d08329cf7a798dd96202a168987
Philipp Sengelmann <philipp.sengelmann@dc-development.de>
2016-04-08 10:26:17 +0200

    test setup require project

---
+++ build.js
@@ -0,0 +1,9 @@
+({
+    baseUrl: "./scripts",
+    paths: {
+        requireLib: 'libs/require'
+    },
+    name: "main",
+    out: "main-built.js",
+    include: ["requireLib"]
+})
---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+require (["helper/util", "helper/other"], function(util, other){
+
+	console.log(util.myObjectName);
+	
+});
+//define("helper/util");
+
+
